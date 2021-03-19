0e8079707a3bebfa36f9ed5f996c55cc344dd0bb
Nyelsonon <51932119+Nyelsonon@users.noreply.github.com>
2021-03-19 14:46:31 -0400

    Update collective.py

--- collective.py
+++ collective.py
@@ -1,72 +1,78 @@
+#imports
 import discord
 from discord.ext import commands
 import pandas
 import csv
 import random
+import simplyJSON as nt
+import json
+import os
 
 #command prefix
 client = commands.Bot(command_prefix='<')
 
-#variables and stuffs
-#df = pandas.DataFrame({'a':['a','b']})
-#df.to_csv('/Users/nealkotval/Desktop/collective bot/output.csv')
-
-@client.command(name="hello")
-async def hello(context):
-    await context.message.channel.send("hey")
-
 @client.command(name="loginfo")
 async def loginfo(context, *args):
+    #remove unecessary symbols like , () and '
     convert = str(args)
     characters_to_remove = ",()'"
     convert1 = convert
     for character in characters_to_remove:
         convert1 = convert1.replace(character, "")
-    print(convert1)
+
+    #notify user
     mention = context.author.mention
     spareembed=discord.Embed(title="Log Submitted!", description='Log: "' + convert1 + '" - info logged by user ' + mention, color=0x000FF)
     await context.message.channel.send(embed=spareembed)
-    df=pandas.read_csv('/Users/nealkotval/Desktop/collective bot/output.csv',index_col=0)
-    df=df.append({'logs':convert1}, ignore_index=True)
-    df.to_csv('/Users/nealkotval/Desktop/collective bot/output.csv')
+
+    #send to json using simplyJSON module
+    nt.appendJSON("output.json",{mention:convert1})
 
 @client.command(name="checklogs")
 async def checklogs(context, lognum):
+    #variables
+    logger = 0
+    logger_logger = 0
 
-    
-    with open('output.csv', 'r') as file:
-        reader = csv.reader(file)
-        a=0
-        if lognum == "random":
-            chosen_row = random.choice(list(reader))
-            bembed = discord.Embed(title="Random Log:", description=str(chosen_row))
-            await context.message.channel.send(embed=bembed)
-                
+    #code for lognum = random
+    if lognum == "random":
+        #initializing JSON file
+        with open('output.json', "r") as f:
+            data = json.load(f)
+            randlog = random.randint(1,len(data))
 
-        for row in reader:
-            a=a+1
-            bruh = str(a)
-            bembed = discord.Embed(title="Log " + str(a) + ":", description=str(row))
+            #finding random log
+            for key, value in data.items():
+                logger_logger = logger_logger + 1
+                if logger_logger == randlog:
+                    chosen_row = value
+                    chosen_author = key
+                    #display for user
+                    bembed = discord.Embed(title="Random Log:", description='"' + str(chosen_row) + '"  - Written by User: ' + str(chosen_author) + ", Log ID: " + str(logger_logger))
+                    await context.message.channel.send(embed=bembed)
+    try:
+        lognum = int(lognum)
+        with open('output.json', 'r') as f2:
+            data = json.load(f2)
 
-            if lognum == "all":
-                await context.message.channel.send(embed=bembed)
-            
-            if lognum == bruh:
-                print(bruh)
-                await context.message.channel.send(embed=bembed)
-        try:
-            lognum = int(lognum)
-        except:
-            print('not a num')
-        else:
-            print(len(list(reader)))
-            file = open("output.csv")
-            reader1 = csv.reader(file)
-            lines = len(list(reader1))
-            if lognum > lines:
-                stringlen = str(lines)
-                sparembed = discord.Embed(title="That log does not exist!", description="Only " + stringlen + " exist...")
-                await context.message.channel.send(embed=sparembed)
+            if lognum > len(data):
+                tryfly = len(data)
+                toolongembed = discord.Embed(title="Log Not Found", description="Only " + str(tryfly) + " logs exist...")
+                await context.message.channel.send(embed=toolongembed)
+            else:
+                for key, value in data.items():
+                        logger = logger + 1
+                        if logger == lognum:
+                            chosen_row = value
+                            chosen_author = key
+                            #display for user
+                            bembed = discord.Embed(title="Found Log:", description='"' + str(chosen_row) + '"  - Written by User: ' + str(chosen_author) + ", Log ID: " + str(logger))
+                            await context.message.channel.send(embed=bembed)
+    except:
+        print("e")
+
+
+
 
 @client.command(name="invite")
 async def invite(context):
@@ -79,4 +85,4 @@ async def invite(context):
 
 
 #bot token
-client.run("-token-")
+client.run("token")
